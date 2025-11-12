2b3dbf2839f931b54f27573518a73477d5bf678b
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-11-12 06:49:51 -0300

    Update: Minor layout changes on header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import { Navigation } from './navigation/Navigation';
 
 export const Header = () => {
   return (
-    <header className="group from-black-primary text-white-primary hover:bg-black-primary shadow-translucid-black px-corners fixed top-0 z-50 flex h-11 w-screen min-w-full justify-between bg-gradient-to-b to-transparent py-1 text-center shadow-lg backdrop-blur-md duration-150 md:h-16 md:py-2">
+    <header className="group from-black-primary text-white-primary hover:bg-black-primary shadow-translucid-black px-corners fixed top-0 z-50 flex h-11 w-screen min-w-full items-center justify-between bg-gradient-to-b to-transparent py-1 text-center shadow-lg backdrop-blur-md duration-150 md:h-16 md:py-2">
       <Logo />
       <Navigation />
     </header>
--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -1,7 +1,11 @@
 export const Logo = () => {
   return (
     <div className="relative flex h-auto w-max opacity-50 transition-opacity duration-150 select-none">
-      <img src="/yuji-logo-white.png" alt="Yuji personal logo" className="cover max-h-16" />
+      <img
+        src="/yuji-logo-white.png"
+        alt="Yuji personal logo"
+        className="cover max-h-9 md:max-h-12"
+      />
     </div>
   );
 };
