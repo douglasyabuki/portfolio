b0387d581e364469c2082b76239add2b1987d9bc
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-11-12 17:22:33 -0300

    Add: ScrollToTop floating button

--- src/components/main-view/MainView.tsx
+++ src/components/main-view/MainView.tsx
@@ -1,8 +1,11 @@
+import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
 import { useOnMount } from '@/hooks/use-on-mount';
 import { useSectionsObserver } from '@/hooks/use-sections-observer';
+import { Icons } from '@/icons/Icons';
 import { initializeLocationId, scrollToCurrentLocationId } from '@/utils/dom-utils';
 import { navigationList } from '@/utils/navigation-list';
 import { useEffect } from 'react';
+import { FloatingButton } from '../ui/buttons/floating-button/FloatingButton';
 import { About } from './about/About';
 import { Contact } from './contact/Contact';
 import { Home } from './home/Home';
@@ -19,6 +22,8 @@ export const MainView = () => {
     return () => window.removeEventListener('hashchange', scrollToCurrentLocationId);
   }, []);
 
+  const { containerRef, isVisible: isPageTopVisible } = useIntersectionObserver();
+
   const { activeId } = useSectionsObserver(
     navigationList.map(({ id }) => id),
     {},
@@ -27,6 +32,7 @@ export const MainView = () => {
 
   return (
     <div className="flex h-full w-screen flex-col items-center py-12" data-active={activeId ?? ''}>
+      <div className="absolute top-0 left-0 size-1 bg-transparent" ref={containerRef} />
       <Home />
       <div className="bg-section-divider-primary" />
       <About />
@@ -36,6 +42,13 @@ export const MainView = () => {
       <Projects />
       <div className="bg-section-divider-secondary" />
       <Contact />
+      <FloatingButton
+        isVisible={!isPageTopVisible}
+        className="group/scroll-to-top-button"
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      >
+        <Icons.ArrowUp className="duration-150 group-hover/scroll-to-top-button:animate-bounce group-focus/scroll-to-top-button:animate-bounce" />
+      </FloatingButton>
     </div>
   );
 };
